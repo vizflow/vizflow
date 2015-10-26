09f02ad00962d8639c983b3a8a7c6101cd8a5549
dannyko <dannykorenblum@gmail.com>
2015-10-26 16:49:13 -0700

    updated dashboard

--- examples/ph_game/ph_game.js
+++ examples/ph_game/ph_game.js
@@ -259,9 +259,9 @@ function ph_game() {
         context.fillText("Current pH: " + pH.toPrecision(3), 10, 50) ;          
         context.fillText("Target pH: " + target.toPrecision(3), 10, 100) ;
         if(pH !== target) {
-          var message = 'Goal: 10            for every' ;
-          draw_water(context, {x: 275 , y: 140, spin: Math.PI / 3}) ;
-          draw_hydronium(context, {x: 540 , y: 140, spin: Math.PI / 3}) ;
+          var message = 'Goal: 10         for every' ;
+          draw_water(context, {x: 190 , y: 140, spin: Math.PI / 3}) ;
+          draw_hydronium(context, {x: 420 , y: 140, spin: Math.PI / 3}) ;
           context.fillStyle = "white" ;
         }
         else {
