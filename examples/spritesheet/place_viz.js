15c9e7775567cd8a825d01c89b6a82e4f1955912
dannyko <dannykorenblum@gmail.com>
2015-12-21 01:10:52 -0800

    spritesheet animation working

--- examples/spritesheet/place_viz.js
+++ examples/spritesheet/place_viz.js
@@ -2,12 +2,12 @@ function place_viz (canvas) {
 
 	document.body.appendChild(canvas) ;
 
-	// function resize() {
-	//   set_canvas_position( canvas ) ;
-	// }
+	function resize() {
+	  set_canvas_position( canvas ) ;
+	}
 
-	// resize() ;
-	// var tResize = 85 ; // how often to check for window resize events (85 is ve 17 ms frames)
-	// setInterval( resize, tResize ) ;
+	resize() ;
+	var tResize = 85 ; // how often to check for window resize events (85 is ve 17 ms frames)
+	setInterval( resize, tResize ) ;
 	
 }
--- examples/spritesheet/spritesheet.js
+++ examples/spritesheet/spritesheet.js
@@ -1,15 +1,20 @@
-document.body.style.overflowY  = 'hidden';
-document.body.style.margin     = 0 ;
+document.body.style.overflowY = 'hidden';
+document.body.style.margin    = 0 ;
 
-var Nrect   = 0 ;
-var canvas  = create_canvas(Nrect) ;
-var context = create_context(canvas) ;
-
-var img       = new Image();
+var canvas    = create_canvas() ;
+var context   = create_context(canvas) ;
+var img       = new Image() ;
+var dur       = 100 ;
 var tileSize  = 16 ;
 var rowIndex  = 6 ;
 var tileCount = 6 ;
-var tiles     = [] ;
+var tile      = [] ;
+
+function draw_image() {
+	var tile = this ;
+	context.drawImage(tile.image, 0 , 0) ;
+}
+
 img.onload = function() {
 	//context.drawImage(img, 0, 0) ;
   canvas.width  = img.width ;
@@ -17,12 +22,35 @@ img.onload = function() {
   context.drawImage(img, 0 , 0) ;
 
   for(var t = 0 ; t < tileCount ; t++) {
-    tiles[t] = context.getImageData(t * tileSize, rowIndex * tileSize, tileSize, tileSize) ;
+  	var image       = context.getImageData(t * tileSize, rowIndex * tileSize, tileSize, tileSize)	 ;
+    var tileCanvas  = create_canvas(tileSize) ;
+    var tileContext = create_context(tileCanvas) ;
+    tileContext.putImageData(image, 0, 0);
+    tile[t] = { image: tileCanvas, render: draw_image } ;
   }
 
   var viz = create_canvas(tileSize) ; 
   place_viz(viz) ;
   context = create_context(viz) ;
-  context.putImageData(tiles[0], 0, 0) ;
+
+  var step_transition = step_transition_func('image', dur) ;
+  //console.log(stepTransition)
+  //console.log(tile)
+
+  var item = [{image: tile[0].image, render: draw_image}] ;
+  $Z.item(item)   ; // load the user data into the visualization engine to initialize the time equals zero (t = 0) state
+	$Z.prep([prep]) ; // sets the preprocessing to perform on each frame of the animation (prior to updating and rendering the elements)
+	$Z.run()        ; // run the interactive visualization (infinite loop by default)
+
+  var counter = 0 ;
+  function loop() {  	
+	  var stepTransition = step_transition( tile[counter % tile.length].image ) ;
+	  item[0].transition = [stepTransition] ;
+	  counter++ ;
+  }
+
+  setInterval(loop, dur * 2) ;
+
 }
-img.src = 'burgertime_spritesheet.png';
+
+img.src = 'burgertime_spritesheet.png' ;
