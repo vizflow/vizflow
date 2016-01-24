f697e0b3ca52d7e1388402b4ddc5173c5f65466d
dannyko <dannykorenblum@gmail.com>
2016-01-24 03:31:03 -0800

    updated ratio to devicePixelRatio instead of hardcoded value used for debugging

--- examples/trump/src/trump/trump_game.js
+++ examples/trump/src/trump/trump_game.js
@@ -1,6 +1,6 @@
 function trump_game () {
   document.skipIndex = 0 ;
-  document.ratio     = 2 ; // (window.devicePixelRatio || 1) ;
+  document.ratio     = (window.devicePixelRatio || 1) ;
 
   city_level() ;
   // fantasy_level() ;
