914a2c4da1a059fdd97d17b85cdf0f207f4a67c4
dannyko <dannykorenblum@gmail.com>
2016-02-13 18:32:15 -0800

    removed some console log statements

--- examples/trump/src/load/load_characters.js
+++ examples/trump/src/load/load_characters.js
@@ -1,9 +1,7 @@
 function load_characters(viz) {
 
   viz.player = setup_element(viz, viz.playerConfig) ;
-  console.log('first') ;
   viz.enemy  = setup_element(viz, viz.enemyConfig) ;
-  console.log('second') ;
   viz.player.orientation = 'r' ; // all players start facing right
 
   viz.player.adversary = viz.enemy ; // decorate the player object for convenient access to the viz.enemy object 
