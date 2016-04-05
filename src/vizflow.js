d50fb5a792671c97d27510089e555d41a033b8db
dannyko <dannykorenblum@gmail.com>
2016-04-05 02:25:44 -0700

    updated whitespace to replace tabs with spaces

--- src/vizflow.js
+++ src/vizflow.js
@@ -44,39 +44,39 @@ window.$Z = { // define the "bling Z" object for running interactive vizualizati
    ),
 
   task: [            // array of functions defining the sequence of asynchronous (non-blocking) tasks to perform for each step/frame/iteration of the visualization
-	  preprocess,      // process user inputs and translate them into actionable changes to the data item attributes
-	  detect_actions,  // apply simulation or game logic e.g. collision detection etc. to determine what actions need to be performed
-	  perform_actions, // perform any actions e.g. item updates that are necessary for the simulation to continue
-	  update_items,    // apply changes to the data item attributes as determined by current data item and user input states 
-	  render_image,    // draw the data items to the screen somehow (render-agnostic)
-	  postprocess,     // e.g. draw all rendered images to the display element
-	  step_or_exit,    // decides whether to generate another frame or to stop the simulation/game
+    preprocess,      // process user inputs and translate them into actionable changes to the data item attributes
+    detect_actions,  // apply simulation or game logic e.g. collision detection etc. to determine what actions need to be performed
+    perform_actions, // perform any actions e.g. item updates that are necessary for the simulation to continue
+    update_items,    // apply changes to the data item attributes as determined by current data item and user input states 
+    render_image,    // draw the data items to the screen somehow (render-agnostic)
+    postprocess,     // e.g. draw all rendered images to the display element
+    step_or_exit,    // decides whether to generate another frame or to stop the simulation/game
   ],
 
   verbose: false,    // toggles console log statements
-	iter:    0,        // initialize loop iteration index (simulation step counter)
-	maxIter: Infinity, // default maximum iteration count allowed (max # of frames
+  iter:    0,        // initialize loop iteration index (simulation step counter)
+  maxIter: Infinity, // default maximum iteration count allowed (max # of frames
 
-	_item:    [], // default data item array (internal use only as marked by underscore)
-	_prep:    [], // array of preprocessing tasks to perform (internal use only as marked by underscore)
-	_post:    [], // array of postprocessing tasks to perform
-	_detect:  [], // array of detectors (internal use only as marked by underscore)
-	_perform: [], // array of actions (internal use only as marked by underscore)
-	_viz:     {}, // optional global visualization configuration object
+  _item:    [], // default data item array (internal use only as marked by underscore)
+  _prep:    [], // array of preprocessing tasks to perform (internal use only as marked by underscore)
+  _post:    [], // array of postprocessing tasks to perform
+  _detect:  [], // array of detectors (internal use only as marked by underscore)
+  _perform: [], // array of actions (internal use only as marked by underscore)
+  _viz:     {}, // optional global visualization configuration object
 	
-	transition, // module comtaining transition helpers
-	item,       // getter/setter function for interfacing with the item/data array
-	prep,       // getter/setter function for interfacing with the _prep array
-	update,     // default update function for items using arrays of transition objects containing interpolation functions
-	post,       // getter/setter function for interfacing with the _prep array
-	detect,     // getter/setter function for interfacing with the _detect array
-	perform,    // getter/setter function for interfacing with the _perform array
-	pipe,       // function for dynamically chaining promises using a for-loop
-	step,       // function that executes one complete step (frame) of the interactive visualization / simulation / game
-	done,       // function to check for the end of the simulation or game, returns true if the simulation or game has ended
-	exit,       // function to execute after the simulation or game has ended to trigger the exit sequence
-	run,        // function that executes each of the asynchronous tasks sequentially using Promise.then() chaining
-	viz,        // function that load the visualization configuration object (viz)
+  transition, // module comtaining transition helpers
+  item,       // getter/setter function for interfacing with the item/data array
+  prep,       // getter/setter function for interfacing with the _prep array
+  update,     // default update function for items using arrays of transition objects containing interpolation functions
+  post,       // getter/setter function for interfacing with the _prep array
+  detect,     // getter/setter function for interfacing with the _detect array
+  perform,    // getter/setter function for interfacing with the _perform array
+  pipe,       // function for dynamically chaining promises using a for-loop
+  step,       // function that executes one complete step (frame) of the interactive visualization / simulation / game
+  done,       // function to check for the end of the simulation or game, returns true if the simulation or game has ended
+  exit,       // function to execute after the simulation or game has ended to trigger the exit sequence
+  run,        // function that executes each of the asynchronous tasks sequentially using Promise.then() chaining
+  viz,        // function that load the visualization configuration object (viz)
 
 } ;
 
