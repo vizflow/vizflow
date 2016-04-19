ea105e7a1d6c16ae48ad0e781ce7515b86dd19c3
korteks <korteks@github.com>
2016-04-18 22:31:33 -0700

    player movement transitions working

--- examples/aerie/src/module/playerHelper.js
+++ examples/aerie/src/module/playerHelper.js
@@ -51,45 +51,95 @@ var playerHelper = {
 
       switch(state) {
 
-        case 'l' :
+ case 'l' :
 
-          var xMin        = -Math.floor(player.sprite.rest[0].originalCanvas.width * 0.5) ;
+          var xMin        = -Math.floor(player.sprite.rest[0].originalCanvas.width * 2.3) ;
           var x           = player.item.x - player.xMove ;
           var xNew        = Math.max(xMin, x) ;
           var xTransition = player.transitionSet.x(xNew) ;      
           player.item.add_transition(xTransition) ;
 
+          var viewXmin = -100 ;
+          var viz = player.item.viz ;
+          var viewTol = 120 ;
+          var center = player.item.image.originalCanvas.width * 0.5 + player.item.x ;
+          var dist = center - viz.viewportX - viewTol ;
+
+          if(dist < 0 && viz.viewportX > viewXmin) {
+            var viewXnew = Math.max(viewXmin, viz.viewportX + dist) ;
+            var replacementSwitch = true ;
+            viz.add_transition(viz.transitionSet.x(viewXnew), replacementSwitch) ;
+          } 
+
           break ;
 
         case 'r' :
 
-          var xMin        = -Math.floor(player.sprite.rest[0].originalCanvas.width * 0.5) ;
+          var xMax        = Math.floor(player.sprite.rest[0].originalCanvas.width * 6) ;
           var x           = player.item.x + player.xMove ;
-          var xNew        = Math.max(xMin, x) ;
+          var xNew        = Math.min(xMax, x) ;
           var xTransition = player.transitionSet.x(xNew) ;      
           player.item.add_transition(xTransition) ;      
+
+          var viewXmax = 100 ;
+          var viz = player.item.viz ;
+          var viewTol = 120 ;
+          var center = player.item.image.originalCanvas.width * 0.5 + player.item.x ;
+          var dist = (viz.viewportX + viz.width) - center ;
+  
+          if( dist < viewTol && viz.viewportX < viewXmax ) {
+            var viewXnew = Math.min(viewXmax, viz.viewportX + (viewTol - dist)) ;
+            var replacementSwitch = true ;
+            viz.add_transition(viz.transitionSet.x(viewXnew), replacementSwitch) ;
+          }
 
           break ;
        
         case 'd' :
 
-          var yMin        = -Math.floor(player.sprite.rest[0].originalCanvas.width * 0.5) ;
+          var yMax        = Math.floor(player.sprite.rest[0].originalCanvas.height * 5) ;
           var y           = player.item.y + player.yMove ;
-          var yNew        = Math.max(yMin, y) ;
+          var yNew        = Math.min(yMax, y) ;
           var yTransition = player.transitionSet.y(yNew) ;      
-          player.item.add_transition(yTransition) ;           
+          player.item.add_transition(yTransition) ;    
+
+          var viewYmax = 100 ;
+          var viz = player.item.viz ;
+          var viewTol = 100 ;
+          var center = player.item.image.originalCanvas.height * 0.5 + player.item.y ;
+          var dist = (viz.viewportY + viz.height) - center ;
+  
+          if( dist < viewTol && viz.viewportY < viewYmax ) {
+            var viewYnew = Math.min(viewYmax, viz.viewportY + (viewTol - dist)) ;
+            var replacementSwitch = true ;
+            viz.add_transition(viz.transitionSet.y(viewYnew), replacementSwitch) ;
+          }
+
           
           break ;
 
         case 'u' :
 
-          var yMin        = -Math.floor(player.sprite.rest[0].originalCanvas.width * 0.5) ;
+          var yMin        = -Math.floor(player.sprite.rest[0].originalCanvas.height * 2.5) ;
           var y           = player.item.y - player.yMove ;
           var yNew        = Math.max(yMin, y) ;
           var yTransition = player.transitionSet.y(yNew) ;      
           player.item.add_transition(yTransition) ;           
 
-          break ;      
+          var viewYmin = -200 ;
+          var viz = player.item.viz ;
+          var viewTol = 150 ;
+          var center = player.item.image.originalCanvas.height * 0.5 + player.item.y ;
+          var dist = center - viz.viewportY ; 
+          // console.log('dist', dist, 'viewTol', viewTol, 'viz.viewportY', viz.viewportY, 'viewYmax', viewYmax) ;
+  
+          if( dist < viewTol && viz.viewportY > viewYmin ) {
+            var viewYnew = Math.max(viewYmin, viz.viewportY - (viewTol - dist)) ;
+            var replacementSwitch = true ;
+            viz.add_transition(viz.transitionSet.y(viewYnew), replacementSwitch) ;
+          }
+
+          break ;       
    
       }
 
