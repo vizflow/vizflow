71eead05a4f6607552d6b57e56d1b6f5380f0ddd
dannyko <dannykorenblum@gmail.com>
2016-04-21 13:32:58 -0700

    removed blank game in favor of basic game

--- examples/template/blank_game/src/animation/transition_sequence.js
+++
@@ -1,7 +0,0 @@
-function transition_sequence(transitionArray) {
-  var transition = transitionArray[0] ;
-  for(var k = 0 ; k < transitionArray.length - 1 ; k++) {
-    transitionArray[k].child = transitionArray[k + 1] ;
-  }
-  return [transition] ;
-}
--- examples/template/blank_game/src/image/get_image_data.js
+++
@@ -1,4 +0,0 @@
-function get_image_data (canvas) {
-  return canvas.context().getImageData(0, 0, canvas.width, canvas.height) ;
-	
-}
