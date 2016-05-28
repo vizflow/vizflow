19247355e571b28d8ebbd3b7d4e5bdab5687103f
awesomizer <christian.working@gmail.com>
2016-05-28 12:39:48 -0400

    fix typo in comment - transition.js

--- src/transition.js
+++ src/transition.js
@@ -9,8 +9,8 @@ var transition = { // module containing transition helper functions
   },
 
   color_interp: function color_interp(t) {
-    var color1 = this.startValue ; // here, "this" revers to whatever context this gets bound to (not this module itself)
-    var color2 = this.endValue ;   // here, "this" revers to whatever context this gets bound to (not this module itself)
+    var color1 = this.startValue ; // here, "this" refers to whatever context this gets bound to (not this module itself)
+    var color2 = this.endValue ;   // here, "this" refers to whatever context this gets bound to (not this module itself)
 
     color1 = color1.slice(1) ; // take off the hash
     color2 = color2.slice(1) ; // take off the hash
