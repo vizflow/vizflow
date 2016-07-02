17f457deb7fb04b7c08fd284cc26b026aa4f313e
dannyko <dannykorenblum@gmail.com>
2016-07-01 21:41:26 -0700

    addd async pipe!

--- src/module/helper/async.js
+++ src/module/helper/async.js
@@ -1,6 +1,6 @@
 let asyncHelper = {
 
-  promisify: function async_helper_promisify(async_func) { // async_func should only accept one or two arguments (resolve and reject, see Promise API for details)
+  promisify: function async_promisify(async_func) { // async_func should only accept one or two arguments (resolve and reject, see Promise API for details)
 
     var p = new Promise(function(resolve) {
       async_func(resolve) ;
@@ -10,7 +10,7 @@ let asyncHelper = {
 
   },
 
-  promise: function setup_promisify(async) { // function that creates functions that return promises that wrap our async functions 
+  promise: function async_promise(async) { // function that creates functions that return promises that wrap our async functions 
 
     function setup_async_promise() {
       return asyncHelper.promisify(async) ;
@@ -18,6 +18,10 @@ let asyncHelper = {
 
     return setup_async_promise ;
 
+  },
+
+  pipe: function async_pipe(asyncList) { // takes an array of async functions and pipes them using async Promises
+    return $Z.pipe( asyncList.map( (d) => asyncHelper.promise(d) ) ) ; // async pipe ftw
   },
 
 } ;
